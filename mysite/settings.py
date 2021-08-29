d2b8f2e85f56b4dfa959f7aa243f63ed1edab769
Prashant Soni <9165prashant@gmail.com>
2021-08-29 13:19:08 +0545

    postgres connected to aws

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'final_db',
+        'USER':'prashant' , 
+        'PASSWORD': 'lastacc1' , 
+        'HOST':'database-2.chyp24iaawts.us-east-2.rds.amazonaws.com' , 
+        'PORT':'5432' , 
     }
 }
 
@@ -119,7 +129,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
